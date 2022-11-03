95cd77ebfb2574384d81d7c42e3da20613511818
bkraske <71412733+bkraske@users.noreply.github.com>
2022-11-02 20:23:51 -0600

    Update to POMDPs.jl-style syntax

--- src/solver.jl
+++ src/solver.jl
@@ -6,6 +6,23 @@ macro until(condition, expression)
     end |> esc
 end
 
+## Solver Struct for POMDPs.jl style
+struct CGCPSolver
+    max_time::Float64
+    τ_inc::Float64
+    ρ::Float64
+end
+
+function CGCPSolver(; max_time=1e5, τ_inc=100.0, ρ=3.0)
+    return CGCPSolver(max_time,τ_inc,ρ)
+end
+
+mutable struct ProbWrap
+    m::ConstrainedPOMDPWrapper
+    λ::Vector{Float64}
+    initialized::Bool
+end
+
 ##need to make this part of core instead of in this file
 function POMDPs.reward(m::ConstrainedPOMDPs.ConstrainedPOMDPWrapper, s, a)
     return m.initialized*reward(m.m, s, a) - m.λ*ConstrainedPOMDPs.cost(m.m,s,a)
@@ -27,12 +44,12 @@ function (sarc::FunctionSARC)(s, a)
     end
 end
 
-function initialize_master(m::ConstrainedPOMDPWrapper, solver, sim::ConstrainedPOMDPs.RolloutSimulator)
+function initialize_master(m::ProbWrap, solver, sim::ConstrainedPOMDPs.RolloutSimulator)
     #check correctness of this
     policy_vector = Vector{AlphaVectorPolicy}(undef, 0)
     mlp = Model(GLPK.Optimizer)
     @variable(mlp, x[1:1] >= 0)
-    λ = 1.0
+    λ = ones(length(m.m.constraints))
     τ = 20.0
     m.initialized = false
     policy = compute_policy(m, solver, λ, τ, 1.0)
@@ -45,7 +62,7 @@ function initialize_master(m::ConstrainedPOMDPWrapper, solver, sim::ConstrainedP
             sum(v*x[i] for i in 1:1)
         )
 
-    @constraint(mlp, dualcon, sum(c*x[1]) <= m.constraints[1])
+    @constraint(mlp, dualcon, sum(c*x[1]) <= m.m.constraints[1])
     @constraint(mlp, validprobability, sum(x[i] for i in 1:1) == 1.0)
 
     return mlp, x, dualcon, validprobability, policy_vector
@@ -63,7 +80,7 @@ function add_column_to_master!(mlp, x, v, c, dualcon, validprobability, ncols)
     set_normalized_coefficient(validprobability, x[ncols], 1)
 end
 
-function evaluate_policy(m, policy, simmer::ConstrainedPOMDPs.RolloutSimulator; parallel=true)
+function evaluate_policy(m::ProbWrap, policy, simmer::ConstrainedPOMDPs.RolloutSimulator; parallel=true)
     #monte carlo simulation evaluation of policy
     #TODO: currently, we use MC evaluation instead of policy graph with lots of
     # need to use policy graph evaluation for comparison
@@ -71,17 +88,17 @@ function evaluate_policy(m, policy, simmer::ConstrainedPOMDPs.RolloutSimulator; 
     total_v = 0.0
     total_c = 0.0
     λ = m.λ
-    m.λ = 0.0
+    m.λ = zeros(length(m.m.constraints))
     m.initialized = true
     if parallel && Threads.nthreads() > 1
         Threads.@threads for i in 1:n_sim
-            v, c = ConstrainedPOMDPs.simulate(simmer, m, policy, updater(policy), initialstate(m), rand(initialstate(m)))
+            v, c = ConstrainedPOMDPs.simulate(simmer, m.m, policy, updater(policy), initialstate(m.m), rand(initialstate(m.m)))
             total_v += v
             total_c += c
         end
     else
         for i in 1:n_sim
-            v, c = ConstrainedPOMDPs.simulate(simmer, m, policy, updater(policy), initialstate(m), rand(initialstate(m)))
+            v, c = ConstrainedPOMDPs.simulate(simmer, m.m, policy, updater(policy), initialstate(m.m), rand(initialstate(m.m)))
             total_v += v
             total_c += c
         end
@@ -90,13 +107,18 @@ function evaluate_policy(m, policy, simmer::ConstrainedPOMDPs.RolloutSimulator; 
     return total_v/n_sim, ceil((total_c/n_sim),digits = 3)
 end
 
-function compute_policy(m::ConstrainedPOMDPWrapper, solver, λ::Float64, τ::Float64, ρ::Float64)
+function compute_policy(m::ProbWrap, solver, λ::Vector{Float64}, τ::Float64, ρ::Float64)
     m.λ = λ
     solver.timeout = τ
-    return SARSOP.solve(solver, m)
+    return SARSOP.solve(solver, m.m)
 end
 
-function CGCPSolver(M::ConstrainedPOMDPWrapper, max_time, τ_inc, ρ)
+function POMDPs.solve(solver::CGCPSolver, pomdp::ConstrainedPOMDPWrapper)
+    M = ProbWrap(pomdp,ones(length(pomdp.constraints)),false)
+    max_time = solver.max_time
+    τ_inc = solver.τ_inc
+    ρ = solver.ρ
+
     #check correctness of this
     solver = SARSOPSolver(precision=1e-3, verbose= true, fast = true)
     simmer = ConstrainedPOMDPs.RolloutSimulator(max_steps = 500, rng=Xoroshiro128Plus(1))
